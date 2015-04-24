01524f775124b2eab5c9925b79d7022f3c62bf4f
Yashar Imanlou <yashari@gmail.com>
2015-04-24 12:28:52 +0430

    Reform IndexBuilder

--- src/Yaim/Mitter/models/IndexBuilder.php
+++ src/Yaim/Mitter/models/IndexBuilder.php
@@ -32,22 +32,24 @@ class IndexBuilder {
 			<div class="row">
 				<div class="col-md-12">
 					<h2>'.$structure['title'].'</h2>
-				</div><!--/col-md-12-->
-			</div><!--/row-->
+				</div>
+			</div>
 
 			<div class="row">
 				<div class="col-md-12">
 					<div class="block-web col-md-12">';
 						
 		$this->html .= \Form::open(array('method' => 'get'));
-		$this->html .= '<div class="col-md-10">'.\Form::text('search', $search_term, array('required', 'class' => 'form-control parsley-validated', 'required', 'parsley-min'=>'1', 'placeholder' => 'Search In Items' )).'</div>';
-		$this->html .= '<div class="col-md-2">'.\Form::submit('Search', array('class' => 'control-label btn btn-primary')).'</div>';
+		$this->html .= '<div class="row">';
+		$this->html .= '<div class="col-sm-10">'.\Form::text('search', $search_term, array('required', 'class' => 'form-control parsley-validated', 'required', 'parsley-min'=>'1', 'placeholder' => 'Search In Items' )).'</div>';
+		$this->html .= '<div class="col-sm-2">'.\Form::submit('Search', array('class' => 'control-label btn btn-primary')).'</div>';
+		$this->html .= '</div>';
 		$this->html .= \Form::close();
 
 		$this->html .='
 					</div>
-				</div><!--/col-md-12-->
-			</div><!--/row-->
+				</div>
+			</div>
 
 			<div class="row mitter">
 				<div class="col-md-12">
@@ -69,20 +71,16 @@ class IndexBuilder {
 
 	public function index_content($rows)
 	{
-		if(empty($rows) || !is_array($rows))
-		{
+		if(empty($rows) || !is_array($rows)) {
 			$this->html .= 'Please first search for a term.';
-		}
-		else
-		{
+		} else {
 			$this->html .= '
 				<table class="table table-striped table-hover table-bordered" id="editable-sample">
 					<thead>
 						<tr>
 							<th><i class="fa fa-edit"></i></th>';
 
-			foreach (head($rows) as $key => $value)
-			{
+			foreach (head($rows) as $key => $value) {
 				$this->html .= '
 							<th>'.$key.'</th>';
 			}
@@ -92,15 +90,13 @@ class IndexBuilder {
 					</thead>
 					<tbody>';
 
-			foreach ($rows as $id => $fields)
-			{
+			foreach ($rows as $id => $fields) {
 				$update_url = action($this->structure['controller'].'@edit', $id);
 				$this->html .= '
 						<tr class="">
 							<td style="width: 15px;"><a class="edit" href="'.$update_url.'">Edit</a></td>';
 
-				foreach ($fields as $field_name)
-				{
+				foreach ($fields as $field_name) {
 					$this->html .= '
 							<td>'.$field_name.'</td>';
 				}
@@ -117,10 +113,10 @@ class IndexBuilder {
 	{
 		$this->html .='
 						</div>
-					</div><!--/porlets-content-->  
-				</div><!--/block-web--> 
-			</div><!--/col-md-12--> 
-		</div><!--/row-->';
+					</div>
+				</div>
+			</div>
+		</div>';
 	}
 
 	public function get()
