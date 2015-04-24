19e145a0735058c689a2e0c46223eed9461d98b2
Yashar Imanlou <yashari@gmail.com>
2015-04-24 11:55:44 +0430

    Dummy fix for functions redeclare error.

--- src/Yaim/Mitter/functions.php
+++ src/Yaim/Mitter/functions.php
@@ -1,56 +1,68 @@
 <?php
-	function mitterGetBetween($pool, $char1="",$char2="") {
-		$temp1 = strpos($pool,$char1) + strlen($char1);
-		$result = substr($pool,$temp1, strlen($pool));
-		$dd=strpos($result,$char2);
+	if(!function_exists('mitterGetBetween')) {
+		function mitterGetBetween($pool, $char1="",$char2="") {
+			$temp1 = strpos($pool,$char1) + strlen($char1);
+			$result = substr($pool,$temp1, strlen($pool));
+			$dd=strpos($result,$char2);
 
-		if($dd == 0) {
-			$dd = strlen($result);
+			if($dd == 0) {
+				$dd = strlen($result);
+			}
+
+			return substr($result,0,$dd);
 		}
-
-		return substr($result,0,$dd);
 	}
 
-	function mitterNullFilter($var) {
-		return ($var !== NULL && $var !== FALSE);
+	if(!function_exists('mitterNullFilter')) {
+		function mitterNullFilter($var) {
+			return ($var !== NULL && $var !== FALSE);
+		}
 	}
 
-	function mitterTimeToSeconds($str_time) {
-		$str_time = preg_replace("/^([\d]{1,2})\:([\d]{2})$/", "00:$1:$2", $str_time);
-		sscanf($str_time, "%d:%d:%d", $hours, $minutes, $seconds);
-		$time_seconds = $hours * 3600 + $minutes * 60 + $seconds;
-		return $time_seconds;
+	if(!function_exists('mitterTimeToSeconds')) {
+		function mitterTimeToSeconds($str_time) {
+			$str_time = preg_replace("/^([\d]{1,2})\:([\d]{2})$/", "00:$1:$2", $str_time);
+			sscanf($str_time, "%d:%d:%d", $hours, $minutes, $seconds);
+			$time_seconds = $hours * 3600 + $minutes * 60 + $seconds;
+			return $time_seconds;
+		}
 	}
 
-	function mitterCookieExpireDate() {
-		return time() + (10 * 365 * 24 * 60 * 60);
+	if(!function_exists('mitterCookieExpireDate')) {
+		function mitterCookieExpireDate() {
+			return time() + (10 * 365 * 24 * 60 * 60);
+		}
 	}
 
-	function mitterFindNestedArrayKey($array, $key)
-	{
-		if(array_key_exists($key, $array))
-			return $array[$key];
+	if(!function_exists('mitterFindNestedArrayKey')) {
+		function mitterFindNestedArrayKey($array, $key)
+		{
+			if(array_key_exists($key, $array))
+				return $array[$key];
 
-		foreach ($array as $value) {
-			if (is_array($value)) {
-				$result = findNestedArrayKey($value, $key);
+			foreach ($array as $value) {
+				if (is_array($value)) {
+					$result = findNestedArrayKey($value, $key);
 
-				if($result) {
-					return $result;
+					if($result) {
+						return $result;
+					}
 				}
 			}
-		}
 
-		return false;
+			return false;
+		}
 	}
 
-	function mitterDeepArrayFilter($array)
-	{
-		$array = array_filter($array, 'nullFilter');
+	if(!function_exists('mitterDeepArrayFilter')) {
+		function mitterDeepArrayFilter($array)
+		{
+			$array = array_filter($array, 'nullFilter');
 
-		foreach ($array as $key => $item) {
-			if(is_array($item))
-				$array[$key] = deepArrayFilter($item);	
+			foreach ($array as $key => $item) {
+				if(is_array($item))
+					$array[$key] = deepArrayFilter($item);	
+			}
+			return $array;
 		}
-		return $array;
 	}
